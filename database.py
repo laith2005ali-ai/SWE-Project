de6dfda317aad8c21e466c96a254b80614951a95
laith2005ali-ai <laith2005ali@gmail.com>
2026-05-31 22:23:50 +0300

    [US1] Add database connection and initialization

---
+++ database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+DATABASE = "splitmate.db"
+
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_db():
+    conn = get_db_connection()
+
+    with open("schema.sql", "r") as file:
+        conn.executescript(file.read())
+
+    conn.commit()
+    conn.close()
